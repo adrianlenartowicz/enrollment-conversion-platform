aaf9e3c3a0d69d502acd622cff80bbdaf885c641
adrianlenartowicz <adrian@lenartowicz.net>
2024-04-08 21:41:39 +0200

    add basic components

---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  scrollToElement() {
+    const targetElement = this.elementRef.nativeElement.querySelector('#targetElement');
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
