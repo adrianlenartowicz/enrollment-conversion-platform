15fefa66c60096417fe4da00f3f93c1099dca3f9
adrianlenartowicz <adrian@lenartowicz.net>
2024-04-16 00:03:49 +0200

    change feature text on landing page

--- src/environments/set-env.js
+++ src/environments/set-env.js
@@ -1,15 +1,12 @@
 const setEnv = () => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
-// Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
-// Load node modules
   const colors = require('colors');
   const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env'
   });
-// `environment.ts` file structure
   const envConfigFile = `export const environment = {
   formAccessKey: '${process.env.ACCESS_KEY}',
   production: true,
