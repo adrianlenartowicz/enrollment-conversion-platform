5c22227aa1e3c5362b5f78739189acd8179bf27c
adrianlenartowicz <adrian@lenartowicz.net>
2024-07-13 15:31:50 +0200

    improve location component styling, add more maps to the subpage

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Placement } from '../../types/placement.type';
 
 @Component({
   selector: 'app-location',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
-
+  @Input() placement: Placement = 'subpage';
 }
