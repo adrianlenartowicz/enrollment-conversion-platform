0244a209b3981a533c8bff2d0bf65fe48ab98cac
adrianlenartowicz <adrian@lenartowicz.net>
2024-07-28 22:45:22 +0200

    first article content and styling improvements

--- src/app/components/article-detail/article-detail.component.ts
+++ src/app/components/article-detail/article-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '../../services/article.service';
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
-  styleUrl: './article-detail.component.css'
+  styleUrl: './article-detail.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ArticleDetailComponent implements OnInit {
   article: any;
