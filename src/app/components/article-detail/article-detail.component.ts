20b97677c99b78142b85f5569bfa13d1722e7129
adrianlenartowicz <adrian@lenartowicz.net>
2024-12-01 18:56:30 +0100

    add dynamic metadata handling for sharing articles on facebook

--- src/app/components/article-detail/article-detail.component.ts
+++ src/app/components/article-detail/article-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '../../services/article.service';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article-detail',
@@ -13,7 +14,8 @@ export class ArticleDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private metaService: Meta
   ) {}
 
   ngOnInit(): void {
@@ -23,5 +25,9 @@ export class ArticleDetailComponent implements OnInit {
         this.article = this.articleService.getArticleById(articleId);
       }
     });
+
+    this.metaService.updateTag({ property: 'og:title', content: this.article.header });
+    this.metaService.updateTag({ property: 'og:description', content: this.article.paragraph });
+    this.metaService.updateTag({ property: 'og:image', content: `${window.location.origin}/assets/articles/${this.article.imageFileName}` });
   }
 }
